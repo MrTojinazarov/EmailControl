bf3862691eb9b0f1b230e585a5bcca305180b951
Sirojiddin <sirojiddintojinazarov59@gmail.com>
2024-11-19 08:28:18 +0500

    Task uploaded

--- app/Http/Controllers/EmailVerificationController.php
+++ app/Http/Controllers/EmailVerificationController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Email_Vertification;
 use Illuminate\Http\Request;
 
-class EmailVertificationController extends Controller
+class EmailVerificationController extends Controller
 {
     /**
      * Display a listing of the resource.
